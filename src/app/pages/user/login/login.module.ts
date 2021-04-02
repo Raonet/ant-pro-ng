c1aaf95df4e62e5112462950527f4ab5882e70c4
饶佳洪 <1027513492@qq.com>
2021-04-02 18:11:44 +0800

    feat: 首页登录

--- src/app/pages/user/login/login.module.ts
+++ src/app/pages/user/login/login.module.ts
@@ -3,10 +3,21 @@ import { UserLoginComponent } from './login.component';
 import { UserLoginRoutingModule } from './user-login-routing.module';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { IconsProviderModule } from './icons-provider.module';
 @NgModule({
   declarations: [UserLoginComponent],
   exports: [UserLoginComponent],
-  imports: [UserLoginRoutingModule, NzInputModule, NzFormModule],
+  imports: [
+    UserLoginRoutingModule,
+    NzInputModule,
+    NzFormModule,
+    NzButtonModule,
+    IconsProviderModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class UserLoginModule {}
